3eeea35348d729674398e7078d4b09b3acd8809d
UJWAL KUMAR <89293303+ujwxl23@users.noreply.github.com>
2023-06-15 22:58:44 +0530

    Update FileShare.sol
    P2P Devices sharing contract.

--- FileShare.sol
+++ FileShare.sol
@@ -15,50 +15,77 @@ contract FileShare is ERC20, Ownable {
     struct Request{
         string description;
         address payable reqProvider;
-        uint256 needId;
+        uint256 needIdDevice;
     }
     mapping (uint=>Request) public requests;
     uint256 public ReqId;
 
     struct Provide{
-        string description;
-        string deviceName;
+        string[] description;
+        string[] deviceName;
         address payable recipient;
-        uint256 space;
+        uint256[] space;
         uint256 timestamp;
-        bool engage;
+        bool[] engage;
     }
     mapping (uint256=>Provide) public providers;
     uint256 public ProId;
 
+    uint256 public count;
+
     function createProvide(string memory _description, string memory _deviceName, uint256 _space) public {
         Provide storage newProvide = providers[ProId];
         ProId++;
-        newProvide.description=_description;
-        newProvide.deviceName=_deviceName;
+        count=0;
+        newProvide.description[count]=_description;
+        newProvide.deviceName[count]=_deviceName;
         newProvide.recipient=payable(msg.sender);
-        newProvide.space=_space;
-        newProvide.engage=false;
+        newProvide.space[count]=_space;
+        newProvide.engage[count]=false;
     }
 
-    function makeRequestToProvider(uint256 _proId, uint256 _spaceReq, string  memory _description)public {
+    function addDevicesByProvider(uint256 _proId, string memory _newDeviceName, uint256 _newspace, string memory _newDescription) public {
+        Provide storage thisProvide=providers[_proId];
+        ++count;
+        thisProvide.description[count]=_newDescription;
+        thisProvide.deviceName[count]=_newDeviceName;
+        thisProvide.space[count]=_newspace;
+        thisProvide.engage[count]=false;
+    }
+
+    function makeRequestToProvider(uint256 _proId, uint256 _spaceReq, string  memory _deviceNameReq) public {
         // require(raisedAmt>=targetprice);
         Provide storage thisProvide=providers[_proId];
-        require(thisProvide.space >= _spaceReq ,"The provider does not have enoungh space left.");
-        require(thisProvide.engage==false,"The provider is already busy, no resources available.");
-        thisProvide.engage=true;
-        thisProvide.timestamp=block.timestamp;
-        Request storage newRequest = requests[ReqId];
-        ReqId++;
-        newRequest.description=_description;
-        newRequest.reqProvider=thisProvide.recipient;
-        newRequest.needId=_proId;
+        uint256 i;
+        for (i=0; i < (thisProvide.deviceName).length; i++){
+            if (thisProvide.deviceName[i] == _deviceNameReq){
+                require(thisProvide.space[i] >= _spaceReq ,"The provider does not have enoungh space left.");
+                require(thisProvide.engage[i]==false,"The provider is already busy, no resources available.");
+                //transferToken()
+                thisProvide.engage[i]=true;
+                thisProvide.timestamp=block.timestamp;
+                Request storage newRequest = requests[ReqId];
+                ReqId++;
+                newRequest.description=_deviceNameReq;
+                newRequest.reqProvider=thisProvide.recipient;
+                newRequest.needIdDevice=_proId;
+            }
+        }
+    }
+
+    function getAllAvailableDevices(uint256 _proIdAvailable) public view returns(string[] memory){
+        Provide storage thisProvide=providers[_proIdAvailable];
+        string[] storage availableDevices;
+        for (uint256 i=0; i < (thisProvide.deviceName).length; i++){
+            if (thisProvide.engage[i]==false){
+                availableDevices.push(thisProvide.deviceName[i]);
+            }
+        }
+        return availableDevices;
     }
 
     // function finishUse();
-    // function getAllAvailableDevice();
     // function approveRequestorUse();
     // function proStop();
-
 
 }
