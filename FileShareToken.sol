20a3331ba1e0701f4f567927ca16fe77e97fa032
UJWAL KUMAR <89293303+ujwxl23@users.noreply.github.com>
2023-10-05 15:00:55 +0530

    Update FileShareToken.sol

--- FileShareToken.sol
+++ FileShareToken.sol
@@ -4,8 +4,12 @@ pragma solidity ^0.8.15;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-contract FileShareToken is ERC20 {
+contract Token is ERC20 {
     constructor() ERC20("shareToken", "STK") {
         _mint(msg.sender, 5000 * 10 ** decimals());
     }
+    function mintToken() external {
+        _mint(msg.sender, 5000 * 10 ** decimals());
+    }
 }
+
